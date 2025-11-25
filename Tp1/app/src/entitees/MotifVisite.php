712f9c3c39ebf1cc0ea073e531cda345f250c100
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-11-25 16:19:54 +0100

    Create MotifVisite.php

---
+++ Tp1/app/src/entitees/MotifVisite.php
@@ -0,0 +1,38 @@
+<?php
+namespace praticiens\entitees;
+
+class MotifVisite {
+
+    private int $id ;
+    private string $libelle ;
+    private string $description ;
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id ;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void {
+        $this->id = $id ;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLibelle(): string {
+        return $this->libelle ;
+    }
+
+    /**
+     * @param string $libelle
+     */
+    public function setLibelle(string $libelle): void {
+        $this->libelle = $libelle ;
+    }
+}
