f01bac66c4ba502490a4ba7e606a63441500e138
Flyguss <crafteur55100-verdun@hotmail.com>
2025-11-25 14:56:08 +0100

    Exo 1 question 1

--- Tp1/app/html/test.php
+++ Tp1/app/html/test.php
@@ -1,4 +1,24 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+use praticiens\entitees\Specialite;
 
 
-echo "Test" ;
+$mapping_path = __DIR__ . '/../infra/mapping/';
+$isDevMode=true;
+$dbParams = parse_ini_file(__DIR__ . '/../config/ticket.ini');
+
+$config = ORMSetup::
+createXMLMetadataConfiguration([$mapping_path], $isDevMode);
+$connection = DriverManager::getConnection($dbParams, $config);
+$entityManager = new EntityManager($connection, $config);
+
+$specialityRepository = $entityManager->getRepository(Specialite::class);
+
+echo "<h1>Exercice 1</h1><h2>Question 1 ./ </h2>" ;
+
+$s = $specialityRepository->find(1) ;
+
+echo $s->getLibelle() ;
---
+++ Tp1/app/src/entitees/Specialite.php
@@ -0,0 +1,56 @@
+<?php
+
+    namespace praticiens\entitees;
+
+class Specialite {
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLibelle(): string
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * @param string $libelle
+     */
+    public function setLibelle(string $libelle): void
+    {
+        $this->libelle = $libelle;
+    }
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    private int $id ;
+    private string $libelle , $description ;
+
+}
