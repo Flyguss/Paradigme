1c0e9507df852808afa7f071c8e7df377dc65ac7
Flyguss <crafteur55100-verdun@hotmail.com>
2025-12-09 16:34:41 +0100

    oui

---
+++ Tp2/app/html/exo3.php
@@ -0,0 +1,25 @@
+<?php
+
+use MongoDB\Client;
+
+require_once __DIR__ . "/../src/vendor/autoload.php";
+
+$c = new Client("mongodb://mongo:27017");
+
+// Sélection de la collection "produits" dans la base par défaut
+$db = $c->local;
+
+
+$produits = $db->produits->find([] , ['projection' => ["_id" => 0 , "numero" => 1 , "categorie" => 1 , "libelle" => 1]]);
+
+$categories = [];
+
+foreach ($produits as $doc) {
+    $categories[] = $doc['categorie'];
+}
+
+$categories = array_unique($categories);
+
+foreach ($categories as $c) {
+    echo "<a href='$c.php'>$c</a><br>" ;
+}
